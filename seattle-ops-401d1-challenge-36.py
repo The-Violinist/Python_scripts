03947361f24669dfbda36236aba0653551d6855b
David Armstrong <62520138+The-Violinist@users.noreply.github.com>
2020-11-24 01:21:31 -0800

    Update seattle-ops-401d1-challenge-36.py

--- seattle-ops-401d1-challenge-36.py
+++ seattle-ops-401d1-challenge-36.py
@@ -9,8 +9,9 @@ import socket
 from telnetlib import Telnet
 import nmap
 ###VARIABLES###
-host = "192.168.1.21"
-#host = input('Please enter an IP address:\n>')
+#host = "192.168.1.21"
+#host = "http://scanme.nmap.org/"
+host = input('Please enter an IP address:\n>')
 port = 22
 #port = input('Please enter a port number:\n>')
 
@@ -35,17 +36,42 @@ def netcat_grab(host, port):
 
 def nmap_grab(host):
     nm = nmap.PortScanner()
-    nm.scan(host, '20-8080')
-    print(f"Port information for {host}")
-    for protocol in nm[host].all_protocols():
-        found_port = nm[host][protocol].keys()
-        for port in found_port:
-            print ('port : %s\tstate : %s' % (port, nm[host][protocol][port]['state']))
+    nm.scan(host, '20-443')
+    for host in nm.all_hosts():
+        print('----------------------------------------------------')
+        print('Host : %s (%s)' % (host, nm[host].hostname()))
+        print('State : %s' % nm[host].state())
+        for proto in nm[host].all_protocols():
+            #print('----------')
+            #print('Protocol : %s' % proto)
+            lport = nm[host][proto].keys()
+            #lport.sort()
+            for port in lport:
+                print ('port : %s\tstate : %s' % (port, nm[host][proto][port]['state']))
+
+
 
 ###MAIN###
-telnet_grab(host, port)
+print("Trying telnet:\n")
+try:
+    telnet_grab(host, port)
+except:
+    print("Sorry, there was a network error.")
+    pass
 time.sleep(2)
-netcat_grab(host, port)
+
+print("Trying netcat:\n")
+try:
+    netcat_grab(host, port)
+except:
+    print("Sorry, there was a network error.")
+    pass
 time.sleep(2)
-nmap_grab(host)
+
+print("Trying nmap:\n")
+try:
+    nmap_grab(host)
+except:
+    print("Sorry, there was a network error.")
+    pass
 ###END###
